9f9a0e7632c21fcfa3cddd5e56e233c5f8d84c4e
Idhibhat Pankam <marginpankam@gmail.com>
2023-05-12 15:28:54 +0700

    feat: implement scene using scenebuilder

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import pane.RootPane;
@@ -9,13 +11,22 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		RootPane rp = new RootPane();
-		Scene scene = new Scene(rp, 800, 640);
-		Stage stage1 = new Stage();
-		stage1.setTitle("PraeAksornSimulator");
-		stage1.setResizable(false);
-		stage1.setScene(scene);
-		stage1.show();
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+			Scene scene = new Scene(root, 800, 640);
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+//		RootPane rp = new RootPane();
+//		Scene scene = new Scene(rp, 800, 640);
+//		Stage stage1 = new Stage();
+//		stage1.setTitle("PraeAksornSimulator");
+//		stage1.setResizable(false);
+//		stage1.setScene(scene);
+//		stage1.show();
 	}
 
 	public static void main(String[] args) {
