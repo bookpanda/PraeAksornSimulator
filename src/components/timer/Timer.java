1e6b71cfac81fbf851b6affada3b2cae8fd19ea4
Idhibhat Pankam <marginpankam@gmail.com>
2023-05-13 14:39:50 +0700

    feat: basic Timer

---
+++ src/components/timer/Timer.java
@@ -0,0 +1,44 @@
+package components.timer;
+
+import javafx.application.Platform;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class Timer extends VBox {
+	private int seconds;
+	private boolean active;
+
+	public Timer() {
+		setSeconds(60);
+		Text text = new Text("Time left");
+		Text timeText = new Text(String.valueOf(seconds));
+		this.getChildren().addAll(text, timeText);
+
+		Thread thread = new Thread(() -> {
+			try {
+				while (seconds > 0) {
+					Thread.sleep(1000);
+					setSeconds(seconds - 1);
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							timeText.setText(String.valueOf(seconds));
+						}
+					});
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		thread.start();
+	}
+
+	public int getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(int seconds) {
+		this.seconds = seconds;
+	}
+}
--- src/pane/RootPane.java
+++ src/pane/RootPane.java
@@ -1,13 +1,17 @@
 package pane;
 
 import components.plate.Plate;
+import components.timer.Timer;
 import javafx.scene.layout.BorderPane;
 
 public class RootPane extends BorderPane {
 	private static Plate plate;
+	private static Timer timer;
 
 	public RootPane() {
 		plate = new Plate();
+		timer = new Timer();
+		this.setTop(timer);
 		this.setCenter(plate);
 	}
 
